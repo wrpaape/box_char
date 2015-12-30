0884dad37d1cdf75c340e4ee61c91f9ea7becfbe
Reid Paape <wrpaape@gmail.com>
2015-12-29 18:45:55 -0600

    still parsing opts

--- lib/box_char/cli.ex
+++ lib/box_char/cli.ex
@@ -2,20 +2,28 @@ defmodule BoxChar.CLI do
   alias IO.ANSI
  # File.stream!(path, [:read, :char_list, encoding: :unicode], :line)   
   @def_opts Application.get_env(:box_char, :def_opts)
-  @parse_opts [
-    mode: [
-      switches: [help:   :boolean, swap:    :boolean, map:  :boolean],
-      aliases:  [h:      :help,    s:       :swap,    m:    :map]
-    ],
-    num_lines: [
-      switches: [single: :boolean, double: :boolean, both: :boolean],
-      aliases:  [s:      :single,  d:       :double, b:    :both]
-    ],
-    line_weight: [
-      switches: [light:  :boolean, heavy:   :boolean, both: :boolean],
-      aliases:  [l:      :light,   h:       :heavy,   b:    :both]
-    ]
-  ]
+  # @parse_opts [
+  #   mode: [
+  #     switches: [help:   :boolean, swap:    :boolean, map:  :boolean],
+  #     aliases:  [h:      :help,    s:       :swap,    m:    :map]
+  #   ],
+  #   num_lines: [
+  #     switches: [single: :boolean, double: :boolean, both: :boolean],
+  #     aliases:  [s:      :single,  d:       :double, b:    :both]
+  #   ],
+  #   line_weight: [
+  #     switches: [light:  :boolean, heavy:   :boolean, both: :boolean],
+  #     aliases:  [l:      :light,   h:       :heavy,   b:    :both]
+  #   ]
+  # ]
+  @parse_opts [help: [switches: [help:  :boolean],
+                      aliases:  [h:     :help]],
+
+               mode: [switches: [map:   :boolean, swap:  :boolean],
+                      aliases:  [m:     :map,     s:     :swap]],
+
+               type: [switches: [light: :boolean, heavy: :boolean, double: :boolean, all: :boolean],
+                      aliases:  [l:     :light,   h:     :heavy,   d:      :double,  a:   :all]]]
 
   def main(argv) do
     argv
@@ -27,107 +35,71 @@ defmodule BoxChar.CLI do
 
   def process({board_size, __}) when board_size in @min..@max, do: process(board_size)
   def process({_board_size, _}), do: alert("board size must be >= #{@min} and <= #{@max}")
-  def process(:error),           do: alert("failed to parse integeer from board size")
-  def process(:help),            do: alert("usage: box_char <path> <mode> <char_set>", :blue)
-  def process(board_size)        do
-    board_size
-    |> Board.start_link
+  def process(:help) do
+    """
+    usage:
 
-    {wrap_dir, turn_str} =
-      "heads or tails (h/t)?"
-      |> Misc.str_app(@cursor)
-      |> IO.gets
-      |> String.match?(coin_flip_reg)
-      |> if do: {:app, "first"}, else: {:pre, "second"}
+      box_char <path> map <type>
 
-    turn_str
-    |> Misc.cap("you will have the ", " move.\nchoose a valid (not whitespace or a number) token character")
-    |> Misc.str_app(@cursor)
-    |> assign_tokens(wrap_dir)
-    |> TicTacToe.start
-  end
+    or
 
-  def parse_args(argv, [| rem_parse_opts]), do: parse_mode([], argv, path_)
-    argv
-    |> OptionParser.parse_head(path_and_mode_parse_opts)
-    |> case do
-      {[help: true], _, _ }  -> :help
-       
-      {_, [], _}             -> @def_opts
- 
-      {_, [size_str | _], _} -> Integer.parse(size_str)
-    end
-  end
-
-  #helpers v  
-
-defmacrop alert(msg, color \\ :red) do
-  quote do
-    apply(ANSI, unquote(color), [])
-    <> unquote(msg)
-    <> ANSI.reset
-    |> IO.puts
+      box_char <path> swap <old_type> <new_type>
+    """
+    |> alert(:blue)
 
     System.halt(0)
   end
-end
-    [
-      thin:  :boolean,
-      thick: :boolean,
-      both:  :boolean]
-  ]
 
-  def main(argv) do
-    argv
-    |> parse_args(@parse_opts)
-    |> process
+  def process({:error, opt, rem_argv}) do
+    """
+    failed to parse <#{opt}> from:
+
+      #{rem_argv}
+    """
+    |> alert(:red)
+
+    process(:help)
   end
 
-  #external API ^
 
-  def process({board_size, __}) when board_size in @min..@max, do: process(board_size)
-  def process({_board_size, _}), do: alert("board size must be >= #{@min} and <= #{@max}")
-  def process(:error),           do: alert("failed to parse integeer from board size")
-  def process(:help),            do: alert("usage: box_char <path> <mode> <char_set>", :blue)
-  def process(board_size)        do
-    board_size
-    |> Board.start_link
-
-    {wrap_dir, turn_str} =
-      "heads or tails (h/t)?"
-      |> Misc.str_app(@cursor)
-      |> IO.gets
-      |> String.match?(coin_flip_reg)
-      |> if do: {:app, "first"}, else: {:pre, "second"}
-
-    turn_str
-    |> Misc.cap("you will have the ", " move.\nchoose a valid (not whitespace or a number) token character")
-    |> Misc.str_app(@cursor)
-    |> assign_tokens(wrap_dir)
-    |> TicTacToe.start
-  end
-
-  def parse_args(argv) do
+  def parse_args(argv, [{:help, help_parse_opts} | rem_parse_opts]) do
     argv
-    |> OptionParser.parse(@parse_opts)
+    |> OptionParser.parse_head(help_parse_opts)
     |> case do
-      {[help: true], _, _ }  -> :help
+      {[help: true], _, _}          -> :help
        
-      {_, [], _}             -> @def_opts
- 
-      {_, [size_str | _], _} -> Integer.parse(size_str)
+      {_, [path_str | rem_argv], _} ->
+        path_str
+        |> Path.wildcard
+        |> Enum.filter(&File.regular?/1)
+        |> case do
+          []    -> {:error, "found no files in path: " <> path_str} 
+
+          files -> parse_args(rem_argv, rem_parse_opts, [files])
+        end
     end
   end
 
+  def parse_args(argv, [{:mode, mode_parse_opts} | rem_parse_opts], parsed_opts) do
+    argv
+    |> OptionParser.parse_head(mode_parse_opts)
+    |> case do
+      {[{mode, true}], rem_argv, _} -> parse_args(rem_argv, rem_parse_opts, [mode | parsed_opts])
+
+      _____________________________ -> {:error, "failed to parse mode"}
+    end
+  end
+
+  def parse_args([], [], parse_opts), do: parsed_opts
+
   #helpers v  
 
-defmacrop alert(msg, color \\ :red) do
-  quote do
-    apply(ANSI, unquote(color), [])
-    <> unquote(msg)
-    <> ANSI.reset
-    |> IO.puts
-
-    System.halt(0)
+  defmacrop alert(msg, color) do
+    quote do
+      apply(ANSI, unquote(color), [])
+      <> unquote(msg)
+      <> ANSI.reset
+      |> IO.puts
+    end
   end
 end
