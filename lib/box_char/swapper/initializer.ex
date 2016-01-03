501ed6c5924cb4ede96b51b864b3e10d0433bf4d
Reid Paape <wrpaape@gmail.com>
2016-01-03 16:19:02 -0600

    WOO stupid sexy patternmatching

--- lib/box_char/swapper/initializer.ex
+++ lib/box_char/swapper/initializer.ex
@@ -4,19 +4,29 @@ defmodule BoxChar.Swapper.Initializer do
   defmacro define_swap_next_functions do
     quote [bind_quoted: [swap_map: @swap_map]] do 
       swap_map
-      |> Enum.each(fn([old_set, new_set, old_char, new_char])->
-        def swap_next(unquote(old_set), unquote(new_set), unquote(old_char) <> rem_str, acc_str) do
+      |> Enum.each(fn([old_set, new_set, << old_char :: utf8 >>, new_char])->
+        IO.inspect([old_set, new_set, old_char, new_char])
+
+        def swap_next(unquote(old_set), unquote(new_set), << unquote(old_char) :: utf8 >> <> rem_str, acc_str) do
+          IO.puts "MAAATCH"
+          IO.inspect([unquote(old_set), unquote(new_set), unquote(old_char), rem_str, acc_str])
           unquote(old_set)
           |> swap_next(unquote(new_set), rem_str, acc_str <> unquote(new_char))
         end
-
-        def swap_next(_, _, << no_match :: binary-size(1) >> <> rem_str, acc_str) do
-          unquote(old_set)
-          |> swap_next(unquote(new_set), rem_str, acc_str <> no_match)
-        end
       end)
 
-      def swap_next(_, _, "", acc_str), do: acc_str
+      def swap_next(old_set, new_set, << no_match :: binary-size(1) >> <> rem_str, acc_str) do
+        IO.puts "NO MATCH"
+        old_set
+        |> swap_next(new_set, rem_str, acc_str <> no_match)
+      end
+      # def swap_next(_, _, "", acc_str), do: acc_str
+
+      def swap_next(_, _, "", acc_str) do
+        IO.puts "finished"
+        IO.inspect(acc_str)
+        acc_str
+      end
     end
   end
 end
