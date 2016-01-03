d1688f114bd0db5cde33bed6b9f8aae5f2d3026f
Reid Paape <wrpaape@gmail.com>
2016-01-03 17:47:12 -0600

    back to tic_tac_toe

--- config/config.exs
+++ config/config.exs
@@ -1,81 +1,5 @@
-defmodule CLIConfig do
-  alias IO.ANSI
-  alias Mix.Project
-  alias BoxChar.CLI.UsageError
-  alias BoxChar.CLI.InputError
-  
-  @spec_delim ","
-  @usage ANSI.blue <> """
-    usage:
-
-      box_char map <escape_open>#{@spec_delim}<escape_close> <path>
-
-    or
-
-      box_char swap <old charset>#{@spec_delim}<new charset> <path>
-    """ <> ANSI.reset
-
-
-  def get_config do
-    Keyword.new
-    |> Keyword.put(:usage, @usage)
-    |> Keyword.put(:spec_delim, @spec_delim)
-    |> Keyword.put(:timeout, :infinity)
-    |> Keyword.put(:root_path, root_path)
-    |> Keyword.put(:parse_opts, parse_opts)
-    |> Keyword.put(:charset_flags, charset_flags)
-    |> Keyword.put(UsageError, usage_errors)
-    |> Keyword.put(InputError, input_errors)
-  end
-
-  defp root_path do
-    ".."
-    |> List.duplicate(4)
-    |> Path.join
-    |> Path.expand(Project.app_path)
-  end
-
-  defp parse_opts do
-    [strict:  [help: :boolean, map: :string, swap: :string],
-     aliases: [h:    :help,    m:   :map,    s:    :swap]]
-  end
-
-  defp charset_flags do
-    [light:  ~w(light l),
-     heavy:  ~w(heavy h),
-     double: ~W(double d),
-     all:    ~w(all a)]
-  end
-
-  defp usage_errors do
-    [missing_mode:         "please specify <mode> of operation",
-     multiple_modes:       "may only operate in one <mode> at a time",
-     missing_path:         "please specify <path> to file or populated directory",
-     extra_args:           "too many args!",
-     no_delim_escapes:     "please delimit your <escapes> with '#{@spec_delim}'",
-     no_delim_charsets:    "please delimit your <escapes> with '#{@spec_delim}'",
-     delim_in_escapes:     "please exclude '#{@spec_delim}' from your <escapes>",
-     missing_swap_charset: "please specify <new charset> to swap with",
-     extra_swap_charsets:  "may only swap one <charset> at a time",
-     same_swap_charsets:   "<new_charset> must be different than <old_charset>",
-     swap_new_charset_all: "<old_charset> cannot be swapped for 'all' <charsets>"]
-    |> Enum.map(fn({error, msg})->
-      {error, msg <> "\n\n" <> @usage}
-    end)
-  end
-
-  defp input_errors do
-    [invalid_path:     "invalid path",
-     unknown_options:  "unknown options",
-     invalid_escapes:  "invalid <escapes>",
-     invalid_charsets: "invalid <charsets>",
-     invalid_charset:  "invalid charset selection",
-     no_files_found:   "failed to find file(s) at path"]
-  end
-end
-
 defmodule MainConfig do
-  def get_config do
+  def get do
     Keyword.new
     |> Keyword.put(:char_map, char_map)
     |> Keyword.put(:swap_map, swap_map)
@@ -132,12 +56,86 @@ defmodule MainConfig do
   end
 end
 
+defmodule CLIConfig do
+  alias IO.ANSI
+  alias Mix.Project
+  alias BoxChar.CLI.UsageError
+  alias BoxChar.CLI.InputError
+  
+  @spec_delim ","
+  @usage ANSI.blue <> """
+    usage:
+
+      box_char map <escape_open>#{@spec_delim}<escape_close> <path>
+
+    or
+
+      box_char swap <old charset>#{@spec_delim}<new charset> <path>
+    """ <> ANSI.reset
+
+  def get do
+    Keyword.new
+    |> Keyword.put(:usage, @usage)
+    |> Keyword.put(:spec_delim, @spec_delim)
+    |> Keyword.put(:root_path, root_path)
+    |> Keyword.put(:parse_opts, parse_opts)
+    |> Keyword.put(:charset_flags, charset_flags)
+    |> Keyword.put(UsageError, usage_errors)
+    |> Keyword.put(InputError, input_errors)
+  end
+
+  defp root_path do
+    ".."
+    |> List.duplicate(4)
+    |> Path.join
+    |> Path.expand(Project.app_path)
+  end
+
+  defp parse_opts do
+    [strict:  [help: :boolean, map: :string, swap: :string],
+     aliases: [h:    :help,    m:   :map,    s:    :swap]]
+  end
+
+  defp charset_flags do
+    [light:  ~w(light l),
+     heavy:  ~w(heavy h),
+     double: ~W(double d),
+     all:    ~w(all a)]
+  end
+
+  defp usage_errors do
+    [missing_mode:         "please specify <mode> of operation",
+     multiple_modes:       "may only operate in one <mode> at a time",
+     missing_path:         "please specify <path> to file or populated directory",
+     extra_args:           "too many args!",
+     no_delim_escapes:     "please delimit your <escapes> with '#{@spec_delim}'",
+     no_delim_charsets:    "please delimit your <escapes> with '#{@spec_delim}'",
+     delim_in_escapes:     "please exclude '#{@spec_delim}' from your <escapes>",
+     missing_swap_charset: "please specify <new charset> to swap with",
+     extra_swap_charsets:  "may only swap one <charset> at a time",
+     same_swap_charsets:   "<new_charset> must be different than <old_charset>",
+     swap_new_charset_all: "<old_charset> cannot be swapped for 'all' <charsets>"]
+    |> Enum.map(fn({error, msg})->
+      {error, msg <> "\n\n" <> @usage}
+    end)
+  end
+
+  defp input_errors do
+    [invalid_path:     "invalid path",
+     unknown_options:  "unknown options",
+     invalid_escapes:  "invalid <escapes>",
+     invalid_charsets: "invalid <charsets>",
+     invalid_charset:  "invalid charset selection",
+     no_files_found:   "failed to find file(s) at path"]
+  end
+end
+
+
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-  config :box_char, CLIConfig.get_config
-    |> Keyword.merge(MainConfig.get_config)
+  config :box_char, Keyword.merge(MainConfig.get, CLIConfig.get)
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
